096094e94567edc41c1a6b27a9dbafdef0428a70
chentianwei <ctw.3@163.com>
2017-02-15 10:45:41 +0800

    delete an item from cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,7 @@
 class CartsController < ApplicationController
 
   def clean
-    current_cart.cart_items.destroy_all
+    current_cart.clean!
     flash[:alert] = "购物车已经清空"
     redirect_to carts_path
   end
